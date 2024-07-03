7ceb2f5c77a490de178872c323df6f2044d0edd7
cristian0196 <cristian.cortes6@icloud.com>
2024-07-02 19:38:44 -0500

    Create bot_actualizado.py

---
+++ bot_actualizado.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+
+
+@bot.command()
+async def test2(ctx, *args):
+    arguments = ', '.join(args)
+    await ctx.send(f'{len(args)} arguments: {arguments}')
+
+
+@bot.command()
+async def test3(ctx, *, arg):
+    await ctx.send(arg)
+
+
+
+bot.run("token")
